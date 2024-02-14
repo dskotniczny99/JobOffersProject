6ebec9d23b17f99ea8d292da946ff71815f57914
Daniel <danielskotniczny1999@gmail.com>
2024-02-14 11:53:05 +0100

    login module

---
+++ src/main/java/com/junioroffers/domain/loginandregister/LoginRepository.java
@@ -0,0 +1,11 @@
+package com.junioroffers.domain.loginandregister;
+
+import java.util.Optional;
+
+interface LoginRepository {
+
+    Optional<User> findByUsername(String username);
+
+    User save(User user);
+
+}
---
+++ src/main/java/com/junioroffers/domain/loginandregister/User.java
@@ -0,0 +1,11 @@
+package com.junioroffers.domain.loginandregister;
+
+import lombok.Builder;
+
+@Builder
+record User(
+        String id,
+        String username,
+        String password
+) {
+}
